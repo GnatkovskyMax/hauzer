76bda9422e2485e9255e88ba47de99b28bf2c870
MaxStepit <gnatkovsky@bigmir.net>
2017-03-03 21:36:07 +0200

    clean

--- core/controllers/admin.php
+++ core/controllers/admin.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 function action_login(){
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -38,37 +38,6 @@ function action_logaut(){
 }
 
 function action_addObject(){
-//if(is_dir('catalog/'.$_POST['address'])){
-//    echo 'hello';
-//    move_uploaded_file($_FILES["picture"]["tmp_name"], "catalog/".$_POST['genre']."/".time().".jpg");
-//    $f = fopen('catalog/file_catalog.php', "a+");
-//    fwrite($f,time().".jpg:".$_POST['genre'].":".$_POST['name'].':'.$_POST['author'].':'.$_POST['price']."\n");
-//    fclose($f);
-//}
-//else{
-//    $d = opendir('catalog');
-//    mkdir('catalog/'.$_POST['genre'],0755,true);
-//    $f = fopen('catalog/file_catalog.php', "a+");
-//    fwrite($f,time().".jpg:".$_POST['genre'].":".$_POST['name'].':'.$_POST['author'].':'.$_POST['price']."\n");
-//    fclose($f);
-//    closedir($d);
-//    move_uploaded_file($_FILES["picture"]["tmp_name"], "catalog/".$_POST['genre']."/".time().".jpg");
-//    echo 'sozdano';
-//}
-//$h = file('catalog/file_catalog.php');
-
-//foreach($h as $g){
-//    $e=explode(':', $g);
-////echo  '<p>'.var_dump($e).'</p>';
-//    $img = $e[0];
-//    $genre = $e[1];
-//    $name = $e[2];
-//    $author = $e[3];
-//    $price = $e[4];
-//    echo '<img src = "catalog/'.$genre.'/'.$img.'">';
-//    echo '<p>'.$name.'</p>';
-//    echo '<p>'.$author.'</p>';
-//    echo '<p>'.$price.'</p>';
     require '/vendor/autoload.php';
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $address=$_POST['city'].' '.$_POST['street'].' '.$_POST['house_number'];
@@ -180,37 +149,6 @@ if($res==true&&$_FILES["image0"]["name"]!=''){
     }else{
     echo 'vvedite vse';
 }
-
-
-       // echo 'sozdano';
-         //FILES['image']["tmp_name"]
-        ////////////////////////////
-//        $jj=$_POST['street'];
-//        $xml = simplexml_load_file("http://maps.google.com/maps/api/geocode/xml?address=$jj");
-//        $status = $xml->status;
-//        $lat = $xml->result->geometry->location->lat;
-//        $lng = $xml->result->geometry->location->lng;
-//        echo $lat.'+'.$lng;
-        //////////////////////////////
-//        $h = $_POST['image']
-//        ?>
-<!--        <p>--><?php //$h ?><!--</p>-->
-<!---->
-<!--        --><?php
-//        $imagine = new Imagine\Gd\Imagine();
-//        $size  = new Imagine\Image\Box(250, 170);
-////        $imagine->open("assets/img/$h")->thumbnail($size, 'inset')->save
-////        ("assets/img/kkk_$h");
-////        $info= pathinfo($h);
-////        var_dump($info['dirname']);
-//        $watermark = $imagine->open("assets/img/hauzer_2017_watermark.png");
-//        $image     = $imagine->open("assets/img/$h");
-//        //$size      = $image->getSize();
-//        $wSize     = $watermark->getSize();
-//
-//        $bottomRight = new Imagine\Image\Point($size->getWidth() - $wSize->getWidth(), $size->getHeight() - $wSize->getHeight());
-//        $image->paste($watermark, $bottomRight)->save
-//        ("assets/img/$h");
     } else {
         renderView('dev/addObject');
     }
